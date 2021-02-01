c100645024d71f34904296728f80c6d8e553fbfc
IzzelAliz <csh2001331@126.com>
2021-02-01 21:19:24 +0800

    update

--- kether-persistent/src/main/java/io/izzel/kether/common/persistent/PersistentLoadError.java
+++ kether-persistent/src/main/java/io/izzel/kether/common/persistent/PersistentLoadError.java
@@ -1,2 +1,14 @@
-package io.izzel.kether.common.persistent;public class PersistentLoadError {
+package io.izzel.kether.common.persistent;
+
+import io.izzel.kether.common.util.LocalizedException;
+
+import java.util.Locale;
+
+public enum PersistentLoadError {
+
+    RECURSIVE_REWRITE;
+
+    public LocalizedException create(Object... args) {
+        return LocalizedException.of("load-error.persistent." + name().toLowerCase(Locale.ROOT), args);
+    }
 }
--- kether-persistent/src/main/java/io/izzel/kether/common/persistent/loader/RewriteLoader.java
+++ kether-persistent/src/main/java/io/izzel/kether/common/persistent/loader/RewriteLoader.java
@@ -3,22 +3,25 @@ package io.izzel.kether.common.persistent.loader;
 import io.izzel.kether.common.api.QuestService;
 import io.izzel.kether.common.loader.SimpleQuestLoader;
 import io.izzel.kether.common.loader.SimpleReader;
+import io.izzel.kether.common.persistent.PersistentLoadError;
+import io.izzel.tools.product.Product3;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class RewriteLoader extends SimpleQuestLoader {
 
     @Override
-    protected Parser newParser(char[] content, QuestService<?> service, Logger logger, List<String> namespace) {
-        return new RewriteParser(content, service, logger, namespace);
+    protected Parser newParser(char[] content, QuestService<?> service, List<String> namespace) {
+        return new RewriteParser(content, service, namespace);
     }
 
     protected static class RewriteParser extends SimpleQuestLoader.Parser {
 
+        private final List<Product3<Integer, Integer, String>> rewriteRules = new ArrayList<>();
         private int lastBegin = -1;
 
-        public RewriteParser(char[] content, QuestService<?> service, Logger logger, List<String> namespace) {
+        public RewriteParser(char[] content, QuestService<?> service, List<String> namespace) {
             super(content, service, namespace);
         }
 
@@ -28,7 +31,13 @@ public class RewriteLoader extends SimpleQuestLoader {
         }
 
         protected void beginRewrite() {
-
+            if (lastBegin == -1) {
+                lastBegin = index;
+            } else {
+                throw PersistentLoadError.RECURSIVE_REWRITE.create();
+            }
         }
+
+        protected
     }
 }
