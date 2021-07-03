98cc3d1c2f9d20875dc858fca6e8548d230278b4
sky <sky@asgard.me>
2021-07-04 00:29:14 +0800

    update

--- kether-common/src/main/java/io/izzel/kether/common/api/ServiceHolder.java
+++ kether-common/src/main/java/io/izzel/kether/common/api/ServiceHolder.java
@@ -1,8 +1,6 @@
 package io.izzel.kether.common.api;
 
-import java.util.ServiceLoader;
-
 public class ServiceHolder {
 
-    static QuestService<?> INSTANCE = ServiceLoader.load(QuestService.class, ServiceHolder.class.getClassLoader()).iterator().next();
+    static QuestService<?> INSTANCE;
 }
